22f4017d88663337b91f67121d179f0efd006734
Max Patsiansky <m.patsianskiy@zmeke.com>
2016-03-17 17:55:08 +0300

    create simple router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react'
+import Admin from '../components/Admin'
+import Genre from '../components/Genre'
+import Home from '../components/Home'
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      route: window.location.hash.substr(1)
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })
+  }
   render() {
-    return <div className='container'>Привет из App!</div>
+    let Child
+
+    switch (this.state.route) {
+      case '/admin/': Child = Admin; break;
+      case '/genre/': Child = Genre; break;
+      default: Child = Home;
+    }
+
+    return (
+      <div className='container'>
+        <h1>App</h1>
+        <ul>
+          <li><a href='#/admin/'>Admin</a></li>
+          <li><a href='#/genre/'>Genre</a></li>
+        </ul>
+        <Child />
+      </div>
+    )
+
   }
 }
