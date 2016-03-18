36b145763e1b36549b926ae7e9e92023fc6493f0
Max Patsiansky <m.patsianskiy@zmeke.com>
2016-03-18 16:55:58 +0300

    change browserHistory to hashHistory

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react-router-course-ru
-Код для раздела [Подключаем react-router](https://maxfarseer.gitbooks.io/react-router-course-ru/content/podklyuchaem_react-router.html)
+Код для раздела [BrowserHistory vs HashHistory](https://maxfarseer.gitbooks.io/react-router-course-ru/content/browserhistory_vs_hashhistory.html)
--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import App from './containers/App'
 import Admin from './components/Admin'
 import Genre from './components/Genre'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='admin' component={Admin} />
       <Route path='genre' component={Genre} />
     </Route>
+    <Route path='*' component={NotFound} />
   </Router>,
   document.getElementById('root')
 )
