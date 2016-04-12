63ddd40ed698a2de68be8d187b630c82ebf6b94b
Max Patsiansky <m.patsianskiy@zmeke.com>
2016-04-12 17:55:50 +0300

    add form with change url button

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react-router-course-ru
-Код для раздела [Ссылка на главную](https://maxfarseer.gitbooks.io/react-router-course-ru/content/ssilka_na_glavnuyu.html)
+Код для раздела [Программируем переходы](https://maxfarseer.gitbooks.io/react-router-course-ru/content/programmiruem_perehodi.html)
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
 
 export default class Home extends Component {
+  handleSubmit(e) {
+    e.preventDefault()
+    const value = e.target.elements[0].value.toLowerCase()
+    browserHistory.push(`/genre/${value}`)
+  }
   render() {
     return (
       <div className='row'>
         <div className='col-md-12'>Раздел /</div>
+        <form className='col-md-4' onSubmit={this.handleSubmit}>
+          <input type='text' placeholder='genreName'/>
+          <button type='submit'>Перейти</button>
+        </form>
       </div>
     )
   }
