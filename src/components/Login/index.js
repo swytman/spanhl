db2998cc6fad804a7d25285b463b4734243e9075
Max Patsiansky <m.patsianskiy@zmeke.com>
2016-04-13 15:31:59 +0300

    redirect after login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 export default class Login extends Component {
+  constructor() {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
   handleSubmit(e) {
     e.preventDefault()
-    const value = e.target.elements[0].value
-    window.localStorage.setItem('rr_login', value)
+    const login = e.target.elements[0].value
+    window.localStorage.setItem('rr_login', login)
+
+    if (login === 'admin') {
+      this.context.router.push('/admin')
+    } else {
+      this.context.router.push('/')
+    }
   }
   render() {
     return (
@@ -17,4 +27,8 @@ export default class Login extends Component {
       </div>
     )
   }
+}
+
+Login.contextTypes = {
+  router: PropTypes.object.isRequired
 }
